6861a2cbcb221f785f4937c0800eba627ad27f61
ilya.grozov <ilya.grozov@rozum.com>
2018-08-31 19:18:47 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Rozum Robotics Machine Learning Research',
+    author='ilya.grozov@rozum.com',
+    license='MIT',
+)
