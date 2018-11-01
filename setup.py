9070c1785464f3880cf7b50c591d7ad4fbeecfc6
ilya.grozov <ilya.grozov@rozum.com>
2018-11-01 20:16:53 +0300

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.0.1',
     description='Rozum Robotics Machine Learning Research',
     author='ilya.grozov@rozum.com',
     license='MIT',
