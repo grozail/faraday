691d5ffb754f60661ecb2c356740f19556c60490
ilya.grozov <ilya.grozov@rozum.com>
2018-11-12 14:58:44 +0300

    added some code

--- src/data/gym/servo.py
+++ src/data/gym/servo.py
@@ -3,12 +3,13 @@ import math
 
 
 class ServoSimulation(object):
-    def __init__(self, kt, jm, jl, cs, cd, fsf, fvf, dt=0.0000625):
+    def __init__(self, kt, jm, jl, cs, cd, fsf, fvf, max_current, dt=0.0000625):
         self.mpa, self.mva, self.lpa, self.lva = 0.0, 0.0, 0.0, 0.0
         self.jm, self.jl, self.cs, self.cd, self.fsf, self.fvf = jm, jl, cs, cd, fsf, fvf
         self.dt = dt
         self.kt = kt
         self.applied_current = 0
+        self.max_current = max_current
 
     def state(self):
         return np.array([self.mpa, self.mva, self.lpa, self.lva])
@@ -20,7 +21,7 @@ class ServoSimulation(object):
         self._step(current, tl)
         return self.state()
 
-    def step_with_current(self, current, tl=0):
+    def step_with_applied_current(self, current, tl=0):
         self._step(current, tl)
         return self.state_with_current()
 
@@ -43,4 +44,4 @@ class ServoSimulation(object):
 
 
 def create_rd50(dt=0.001):
-    return ServoSimulation(0.04, 1.2e-4, 1.2e-4, 0.47, 5e-3, 0.042, 2e-5, dt)
+    return ServoSimulation(0.04, 1.2e-4, 1.2e-4, 0.47, 5e-3, 0.042, 2e-5, 10, dt)
--- src/data/gym/trajectory.py
+++ src/data/gym/trajectory.py
@@ -7,6 +7,7 @@ from .servo import ServoSimulation
 class ServoTrajectoryGenerator(object):
     def __init__(self, servo: ServoSimulation):
         self.servo = servo
+        self.trajectories = []
 
     def __check_traj_time_more_than_dt(self, time_s):
         if time_s < self.servo.dt:
@@ -19,16 +20,32 @@ class ServoTrajectoryGenerator(object):
     def to_dataframe(data):
         return pd.DataFrame(data=data, columns=['mpa', 'mva', 'lpa', 'lva', 'applied_current'])
 
-    def generate_with_constant_current_no_load(self, current, time_s):
+    def generate_with_const_current_no_load(self, current, time_s):
         self.__check_traj_time_more_than_dt(time_s)
         number_of_steps = self.__calculate_number_of_steps(time_s)
-        states = [self.servo.step_with_current(current) for _ in range(number_of_steps)]
-        return self.to_dataframe(np.array(states))
+        states = [self.servo.step_with_applied_current(current) for _ in range(number_of_steps)]
+        trajectory = np.array(states)
+        self.trajectories.append(trajectory)
+        return trajectory
 
+    def generate_with_noisy_uniform_const_current_no_load(self, current, time_s, lower_bound, upper_bound):
+        self.__check_traj_time_more_than_dt(time_s)
+        number_of_steps = self.__calculate_number_of_steps(time_s)
+        noise = np.random.uniform(lower_bound, upper_bound, number_of_steps)
+        states = [self.servo.step_with_applied_current(current + n) for n in noise]
+        trajectory = np.array(states)
+        self.trajectories.append(trajectory)
+        return trajectory
 
+    def generate_with_noisy_gaussian_const_current_no_load(self, current, time_s, mean, std):
+        self.__check_traj_time_more_than_dt(time_s)
+        number_of_steps = self.__calculate_number_of_steps(time_s)
+        noise = np.random.normal(mean, std, number_of_steps)
+        states = [self.servo.step_with_applied_current(current + n) for n in noise]
+        trajectory = np.array(states)
+        self.trajectories.append(trajectory)
+        return trajectory
 
-def generate_df_using_constant_current(servo: ServoSimulation, current, time_s):
-    if time_s < servo.dt:
-        raise ValueError("time_s < servo.dt")
-    number_of_steps = math.ceil(time_s / servo.dt)
+    def clear_trajectories(self):
+        self.trajectories.clear()
 
