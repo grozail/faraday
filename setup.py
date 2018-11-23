4bb96a5fc4dc2faf70a39797e8814064b53f5ef3
ilya.grozov <ilya.grozov@rozum.com>
2018-11-23 22:00:15 +0300

    upd desc

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.0.1',
-    description='Rozum Robotics Machine Learning Research',
+    description=' Machine Learning for Optimal Control Research',
     author='ilya.grozov@rozum.com',
     license='MIT',
 )
